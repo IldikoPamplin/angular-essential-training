0b41078b830a54964868ad3271bf1da9bcb32e7f
IldikoPamplin <98354996+IldikoPamplin@users.noreply.github.com>
2022-03-29 15:22:08 +1000

    02_03 during

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+//bootstrap logic
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
